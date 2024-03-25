8986076c4d3b7431ff4e9e032c5166965ff1a70f
sjr <s1075671577@gmail.com>
2024-03-25 12:12:48 +0000

    modified infer script

--- infer_dist.sh
+++ infer_dist.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-data_ids=("052" "055" "056" "059" "060" "061" "062")
+data_ids=("130")
 total_gpus=8
 i=0
 
 for data_id in "${data_ids[@]}"; do
-    for camera in front rear right left; do    
+    for camera in front rear right left; do
         gpu_id=$((i % total_gpus)) 
 
         if [ ! -d "output/output_${data_id}" ]; then
@@ -16,12 +16,12 @@ for data_id in "${data_ids[@]}"; do
             mkdir "output/output_${data_id}/camera_${camera}"
         fi
 
-        CUDA_VISIBLE_DEVICES=$gpu_id python demo.py --img_folder "/mnt/mnt_0/data/inhouse/ovon/training/topics/${data_id}/camera_${camera}" --out_folder "output/output_${data_id}/camera_${camera}" &
+        CUDA_VISIBLE_DEVICES=$gpu_id python demo.py --img_folder "${data_id}/camera_${camera}" --out_folder "output/output_${data_id}/camera_${camera}" &
 
         ((i++))
         if (( i % (total_gpus * 4) == 0 )); then
             wait
-        fi
+        fi      
     done
 done
 
