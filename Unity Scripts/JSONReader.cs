74ccf1f0a5bdf90a7b2796224b22bdb92d8c166b
sebopab24 <53013616+sebastianopazo1@users.noreply.github.com>
2024-07-18 00:22:09 -0400

    Update JSONReader

--- Unity Scripts/JSONReader.cs
+++ Unity Scripts/JSONReader.cs
@@ -5,37 +5,39 @@ using Newtonsoft.Json;
 
 public class JSONReader
 {
+    public class Human
+    {
+        public List<float> location { get; set; }
+        public List<float> translation { get; set; }
+        public List<List<float>> translation_pelvis { get; set; }
+        public List<List<float>> rotation_vector { get; set; }
+        public List<float> expression { get; set; }
+        public List<float> shape { get; set; }
+        public List<List<float>> joints_2d { get; set; }
+        public List<List<float>> joints_3d { get; set; }
+    }
+
     public class Data
     {
-        public List<List<float>> pose { get; set; }
-        public List<float> shape { get; set; }
-        public List<float> expression { get; set; }
-        public List<float> location { get; set; }
-        public float distance { get; set; }
-        public List<float> rotation { get; set; }
-        public List<float> positionOffset { get; set; }
-        public List<float> rotationOffset { get; set; }
-        public Camera camera { get; set; }
-        public List<List<float>> joints2D { get; set; }
+        public int image_width { get; set; }
+        public int image_height { get; set; }
+        public int resized_width { get; set; }
+        public int resized_height { get; set; }
+        public int checkpoint_resolution { get; set; }
+        public List<List<float>> camera_intrinsics { get; set; }
+        public List<Human> humans { get; set; }
     }
 
-    public class Camera
-    {
-        public List<float> focalLength { get; set; }
-        public List<float> principalPoint { get; set; }
-        public List<int> imageSize { get; set; }
-    }
-
-    private List<Data> dataList;
+    private Data data;
 
     public void ReadJSON(string filePath)
     {
         string json = File.ReadAllText(filePath);
-        dataList = JsonConvert.DeserializeObject<List<Data>>(json);
+        data = JsonConvert.DeserializeObject<Data>(json);
     }
 
-    public List<Data> GetData()
+    public Data GetData()
     {
-        return dataList;
+        return data;
     }
 }
